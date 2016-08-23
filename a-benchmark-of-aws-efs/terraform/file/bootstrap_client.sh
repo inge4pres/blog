15402c653ec7cae33de99ffc51a9ede7629df1a3
Francesco Gualazzi (acn) <fgualazzi@gmail.com>
2016-08-23 08:30:25 +0200

    Adding fio to client bootstrap

--- a-benchmark-of-aws-efs/terraform/file/bootstrap_client.sh
+++ a-benchmark-of-aws-efs/terraform/file/bootstrap_client.sh
@@ -1,6 +1,7 @@
 #install glusterfs repo for stable version
+mkdir -p /mnt/{gluster,efs}
 wget -P /etc/yum.repos.d https://download.gluster.org/pub/gluster/glusterfs/3.7/LATEST/EPEL.repo/glusterfs-epel.repo
 #fix it for amazon linux
 sed -i 's/$releasever/6/g' /etc/yum.repos.d/glusterfs-epel.repo
 #install
-yum install -y glusterfs-fuse
+yum install -y glusterfs-fuse fio
