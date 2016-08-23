99ecb32b1706fbf6dc4d366fdf6df00f96c17c93
Francesco Gualazzi (acn) <fgualazzi@gmail.com>
2016-08-23 08:27:41 +0200

    Fixing bootstrap userdata

--- a-benchmark-of-aws-efs/terraform/file/bootstrap_server.sh
+++ a-benchmark-of-aws-efs/terraform/file/bootstrap_server.sh
@@ -18,4 +18,4 @@ wget -P /etc/yum.repos.d https://download.gluster.org/pub/gluster/glusterfs/3.7/
 #fix it for amazon linux
 sed -i 's/$releasever/6/g' /etc/yum.repos.d/glusterfs-epel.repo
 #install
-yum install -y glusterfs{-fuse,-server}
+yum install -y glusterfs-fuse glusterfs-server
